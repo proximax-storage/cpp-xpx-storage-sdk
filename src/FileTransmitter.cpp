700196ec5db33553bedef5b2a7bab4a137b19772
Viktor.Markvardt <viktor.markvardt@gmail.com>
2021-03-10 19:40:35 +0200

    Implemented addFile

--- src/FileTransmitter.cpp
+++ src/FileTransmitter.cpp
@@ -6,10 +6,23 @@
 
 #include "FileTransmitter.h"
 
+// libtorrent
+#include <libtorrent/alert.hpp>
+#include <libtorrent/alert_types.hpp>
 #include <libtorrent/session.hpp>
 #include <libtorrent/magnet_uri.hpp>
 #include <libtorrent/hex.hpp>
+#include <libtorrent/create_torrent.hpp>
+#include <libtorrent/torrent_flags.hpp>
+#include <libtorrent/extensions/ut_metadata.hpp>
+
+// std
 #include <iostream>
+
+// boost
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 namespace xpx_storage_sdk {
 
@@ -17,14 +30,81 @@ namespace xpx_storage_sdk {
 class DefaultFileTransmitter : public FileTransmitter {
 private:
     lt::session mSession;
+    lt::file_storage mFileStorage;
+
+    std::map<lt::sha256_hash, DownloadFileHandler> mTorrentHandlers;
 
 private:
-    static std::string makeMagnetURI(const FileHash& hash)
-    {
-        std::string res = "magnet:?xt=urn:btmh:1220";
-        res += lt::aux::to_hex(*hash.data());
+    static std::string makeMagnetURI(const FileHash& hash) {
+        const std::string magnetPart = "magnet:?xt=urn:btmh:1220";
+        const std::string hashPart(std::begin(hash), std::end(hash));
 
-        return res;
+        return magnetPart + hashPart;
+    }
+
+    static FileHash toHash(const lt::sha256_hash& hash) {
+        FileHash finalHash;
+        std::copy(hash.begin(), hash.end(), std::begin(finalHash));
+
+        return finalHash;
+    }
+
+    static lt::sha256_hash toLtHash(const FileHash& hash) {
+        lt::sha256_hash finalHash;
+        std::copy(hash.begin(), hash.end(), std::begin(finalHash));
+
+        return finalHash;
+    }
+
+    void alertHandler() {
+        std::vector<lt::alert *> alerts;
+        mSession.pop_alerts(&alerts);
+
+        // TODO: check errors, alerts
+        for (auto &alert : alerts) {
+            switch (alert->type()) {
+                case lt::torrent_finished_alert::alert_type: {
+                    auto *pa = dynamic_cast<lt::torrent_finished_alert *>(alert);
+                    if (pa) {
+                        const lt::sha256_hash ltHash = pa->handle.info_hashes().v2;
+                        if (mTorrentHandlers.contains(ltHash)) {
+
+                            const std::string fileName = pa->handle.torrent_file()->files().file_name(0).to_string();
+                            mSession.remove_torrent(pa->handle);
+
+                            DownloadFileHandler handler = mTorrentHandlers[ltHash];
+                            handler(download_status::complete, toHash(ltHash), fileName);
+
+                            mTorrentHandlers.erase(ltHash);
+                        }
+                    }
+                    break;
+                }
+//                case lt::piece_finished_alert::alert_type: {
+//                    auto *pa = dynamic_cast<lt::piece_finished_alert *>(alert);
+//                    if (pa) {
+//                        const lt::sha256_hash ltHash = pa->handle.torrent_file()->info_hashes().v2;
+//                        std::cout << "alert: curr piece" << pa->piece_index << std::endl;
+//                        std::cout << "alert: num pieces" << pa->handle.torrent_file()->num_pieces() << std::endl;
+//                        std::vector<int64_t> fp;
+//                        pa->handle.file_progress(fp, lt::torrent_handle::piece_granularity);
+//                        std::cout << "alert: my progress: " << fp << std::endl;
+//                    }
+//
+//                    std::cout << "alert: my piece finished: " << alert->message() << std::endl;
+//                    break;
+//                }
+//                case lt::file_completed_alert::alert_type: {
+//                    //auto *pa = dynamic_cast<lt::file_completed_alert *>(alert);
+//
+//                    std::cout << "alert: file completed: " << alert->message() << std::endl;
+//                    break;
+//                }
+                default: {
+                    std::cout << "alert: " << alert->message() << std::endl;
+                }
+            }
+        }
     }
 
 public:
@@ -32,29 +112,22 @@ public:
 
     virtual ~DefaultFileTransmitter() {}
 
-    virtual void init(unsigned short port = 0) {
+    virtual void init( const std::string& address ) {
         lt::settings_pack settingsPack;
 
         settingsPack.set_int(lt::settings_pack::alert_mask, lt::alert_category::all);
-        settingsPack.set_str(lt::settings_pack::dht_bootstrap_nodes, "10.0.0.14:6900");
         settingsPack.set_str(lt::settings_pack::dht_bootstrap_nodes, "");
-        settingsPack.set_str(lt::settings_pack::user_agent, "torrent-client-v0.0.1");
+
+        boost::uuids::uuid uuid = boost::uuids::random_generator()();
+
+        settingsPack.set_str(lt::settings_pack::user_agent, boost::uuids::to_string(uuid));
         settingsPack.set_bool(lt::settings_pack::enable_dht, true);
         settingsPack.set_bool(lt::settings_pack::enable_lsd, true);
-        settingsPack.set_str(lt::settings_pack::listen_interfaces, "10.0.0.15:6800");
+        settingsPack.set_str(lt::settings_pack::listen_interfaces, address);
         settingsPack.set_bool(lt::settings_pack::allow_multiple_connections_per_ip, true);
 
         mSession.apply_settings(settingsPack);
-        mSession.set_alert_notify([&]() {
-            std::vector<lt::alert *> alerts;
-
-            mSession.pop_alerts(&alerts);
-
-            for (auto &alert : alerts) {
-                std::cout << "alert msg: " << alert->message() << std::endl;
-                std::cout << "alert msg: " << alert->what() << std::endl;
-            }
-        });
+        mSession.set_alert_notify([this] { alertHandler(); });
     }
 
     FileHash prepareActionListToUpload( const ActionList&, std::string addr, int port) override {
@@ -62,101 +135,70 @@ public:
         return FileHash();
     }
 
-    void download( FileHash hash, const std::string& outputFolder, DownloadFileHandler, const std::string& addr = "", unsigned short port = 0 ) override {
-
-        if( outputFolder.empty() ) {
+    void download( FileHash hash, const std::string& outputFolder, DownloadFileHandler handler, const std::string& address, unsigned short port ) override {
+        if (outputFolder.empty()) {
+            handler(download_status::failed, hash, "");
             return;
         }
 
-        if( addr.empty() ) {
+        if (address.empty()) {
+            handler(download_status::failed, hash, "");
             return;
         }
 
-        if( port <= 1024 ) {
-            return;
-        }
-
-        // "magnet:?xt=urn:btmh:1220fa63b7c86a9b6086d9640246fbf3a1dd127db8dc01065976a085bdde25f0f101";
         const std::string magnetLink = makeMagnetURI(hash);
 
-        lt::add_torrent_params p = lt::parse_magnet_uri(magnetLink);
-        p.save_path = outputFolder;
+        lt::error_code ec;
+        lt::add_torrent_params tp = lt::parse_magnet_uri(magnetLink, ec);
+        if (ec.value() != 0) {
+            handler(download_status::failed, hash, "");
+            return;
+        }
 
-        lt::torrent_handle th = mSession.add_torrent(p);
+        tp.save_path = outputFolder;
 
-        lt::tcp::endpoint endpoint;
-        endpoint.address(boost::asio::ip::make_address(addr));
-        endpoint.port(port);
+        lt::torrent_handle th = mSession.add_torrent(tp, ec);
+        if (ec.value() != 0) {
+            handler(download_status::failed, hash, "");
+            return;
+        }
 
-        th.connect_peer(endpoint);
+        if (mSession.is_valid() && th.is_valid()) {
+            mTorrentHandlers.insert(std::pair<lt::sha256_hash, DownloadFileHandler>(toLtHash(hash), handler));
 
-//        std::vector<std::int64_t> const file_progress = th.file_progress();
-//        std::vector<lt::open_file_state> file_status = th.file_status();
-//        std::vector<lt::download_priority_t> file_prio = th.get_file_priorities();
-//        auto f = file_status.begin();
-//        std::shared_ptr<const lt::torrent_info> ti = th.torrent_file();
-//
-//        int p = 0; // this is horizontal position
-//        for (lt::file_index_t i(0); i < lt::file_index_t(ti->num_files()); ++i)
-//        {
-//            auto const idx = std::size_t(static_cast<int>(i));
-//            if (pos + 1 >= terminal_height) break;
-//
-//            bool const pad_file = ti->files().pad_file_at(i);
-//            if (pad_file && !show_pad_files) continue;
-//
-//            int const progress = ti->files().file_size(i) > 0
-//                                 ? int(file_progress[idx] * 1000 / ti->files().file_size(i)) : 1000;
-//            assert(file_progress[idx] <= ti->files().file_size(i));
-//
-//            bool const complete = file_progress[idx] == ti->files().file_size(i);
-//
-//            std::string title = ti->files().file_name(i).to_string();
-//            if (!complete)
-//            {
-//                std::snprintf(str, sizeof(str), " (%.1f%%)", progress / 10.0);
-//                title += str;
-//            }
-//
-//            if (f != file_status.end() && f->file_index == i)
-//            {
-//                title += " [ ";
-//                if ((f->open_mode & lt::file_open_mode::rw_mask) == lt::file_open_mode::read_write) title += "read/write ";
-//                else if ((f->open_mode & lt::file_open_mode::rw_mask) == lt::file_open_mode::read_only) title += "read ";
-//                else if ((f->open_mode & lt::file_open_mode::rw_mask) == lt::file_open_mode::write_only) title += "write ";
-//                if (f->open_mode & lt::file_open_mode::random_access) title += "random_access ";
-//                if (f->open_mode & lt::file_open_mode::sparse) title += "sparse ";
-//                title += "]";
-//                ++f;
-//            }
-//
-//            const int file_progress_width = pad_file ? 10 : 65;
-//
-//            // do we need to line-break?
-//            if (p + file_progress_width + 13 > terminal_width)
-//            {
-//                out += "\x1b[K\n";
-//                pos += 1;
-//                p = 0;
-//            }
-//
-//            std::snprintf(str, sizeof(str), "%s %7s p: %d ",
-//                          progress_bar(progress, file_progress_width
-//                                  , pad_file ? col_blue
-//                                             : complete ? col_green : col_yellow
-//                                  , '-', '#', title.c_str()).c_str()
-//                    , add_suffix(file_progress[idx]).c_str()
-//                    , static_cast<std::uint8_t>(file_prio[idx]));
-//
-//            p += file_progress_width + 13;
-//            out += str;
-//        }
+            lt::tcp::endpoint endpoint;
+            endpoint.address(boost::asio::ip::make_address(address));
+            endpoint.port(port);
 
-
+            th.connect_peer(endpoint);
+        } else {
+            handler(download_status::failed, hash, "");
+        }
     }
 
-    void addFile( Key drivePubKey, FileHash, std::string fileNameWithPath, ErrorHandler )  override {
+    void addFile( Key drivePubKey, std::string fileNameWithPath, ErrorHandler handler )  override {
+        lt::add_files(mFileStorage, fileNameWithPath);
 
+        const int piece_size = 16;
+        lt::create_torrent t(mFileStorage, piece_size, lt::create_torrent::v2_only);
+        lt::set_piece_hashes(t, "./files");
+
+        std::vector<char> buf;
+        lt::bencode(std::back_inserter(buf), t.generate());
+
+        lt::add_torrent_params tp;
+        tp.flags &= ~lt::torrent_flags::paused;
+        tp.flags &= ~lt::torrent_flags::auto_managed;
+        tp.storage_mode = lt::storage_mode_sparse;
+        tp.save_path = "./files";
+        tp.ti = std::make_shared<lt::torrent_info>(buf, lt::from_span);
+
+        lt::error_code ec;
+        mSession.add_torrent(tp);
+        if (ec.value() != 0) {
+            handler(error::failed, ec.message());
+            return;
+        }
     }
 
     void removeFile( Key drivePubKey, FileHash, std::string fileNameWithPath, ErrorHandler )  override {
