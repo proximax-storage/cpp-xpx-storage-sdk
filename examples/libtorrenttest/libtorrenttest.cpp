12e75b6f7df442ca56adcae1e82d97e443aaa373
Alex Tsar <tsar1963@gmail.com>
2021-03-15 16:52:47 +0200

    test branch switching

--- examples/libtorrenttest/libtorrenttest.cpp
+++ examples/libtorrenttest/libtorrenttest.cpp
@@ -8,7 +8,7 @@ using namespace xpx_storage_sdk;
 int main(int argc, char *argv[]) {
 
     //InfoHash infoHash = createTorrentFile( std::string pathToFolderOrFolder, std::string outputTorrentFilename = "" );
-
+    // test
 
     auto ltWrapper = createDefaultLibTorrentWrapper();
 
