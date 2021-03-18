c42ab82e55d06042eabfe4441789cfa8d9b80eb6
Alex Tsar <tsar1963@gmail.com>
2021-03-18 17:34:15 +0200

    added comments

--- examples/downloadFile/downloadFile.cpp
+++ examples/downloadFile/downloadFile.cpp
@@ -21,40 +21,52 @@ namespace fs = std::filesystem;
 
 using namespace xpx_storage_sdk;
 
+
 // forward declarations
+//
 void client( endpoint_list replicatorAddresses, InfoHash infoHashOfSomeFile, fs::path destinationFolder );
 void replicator( InfoHash& outInfoHashOfSomeFile );
 fs::path createReplicatorFile();
 
+
 // condition variable and auxiliary variables
+//
 std::condition_variable finishCondVar;
 std::mutex              finishMutex;
 bool                    isFinished = false;
 
+//
 // main
+//
 int main(int,char**)
 {
-    // start replicator and receive InfoHash of some file
+    // Start replicator and receive InfoHash of any file
+    //
     InfoHash hashOfSomeFile;
     std::thread replicatorThread( [&hashOfSomeFile] { replicator( hashOfSomeFile ); });
 
-    // wait 'replicator is ready'
+    // Wait replicator initializing
+    //
     sleep(3);
 
-    // prepare destination folder, where the file will be saved
+    // Prepare destination folder, where the file will be saved
+    //
     auto dstFolder = fs::temp_directory_path() / "downloader_files";
     fs::remove_all( dstFolder );
     fs::create_directories( dstFolder );
 
-    // make the list of replicator addresses
+    // Make the list of replicator addresses
+    //
     endpoint_list addrList;
     boost::asio::ip::address e = boost::asio::ip::address::from_string("127.0.0.1");
     addrList.emplace_back( e, 5550 );
 
-    // run client
+    // Run client
+    //
     client( addrList, hashOfSomeFile, dstFolder );
 
-    // print the result
+    // Print the result
+    //
     if ( isFinished ) {
         std::cout << "successfuly completed" << std::endl;
     }
@@ -62,13 +74,16 @@ int main(int,char**)
         std::cout << "download failed" << std::endl;
     }
 
-    // wait thread
+    // Wait thread
+    //
     replicatorThread.join();
 
     return 0;
 }
 
+//
 // progressHandler
+//
 void progressHandler( download_status::code code, InfoHash, const std::string& info )
 {
     if ( code == download_status::complete ) {
@@ -82,50 +97,62 @@ void progressHandler( download_status::code code, InfoHash, const std::string& i
     }
 }
 
+//
 // client
+//
 void client( endpoint_list replicatorAddresses, InfoHash infoHashOfSomeFile, fs::path destinationFolder )
 {
-    // create libtorrent session
+    // Create libtorrent session
+    //
     auto ltWrapper = createDefaultLibTorrentSession( CLIENT_IP_ADDR ":5551" );
 
-    // start file downloading
+    // Start file downloading
+    //
     ltWrapper->downloadFile( infoHashOfSomeFile,
                              destinationFolder,
                              progressHandler,
                              replicatorAddresses );
 
-    // wait for the download to finish
+    // Wait for the download to finish
+    //
     std::unique_lock<std::mutex> lock(finishMutex);
     finishCondVar.wait( lock, []{return isFinished;} );
 }
 
+//
 // replicator
+//
 void replicator( InfoHash& outInfoHashOfSomeFile )
 {
     fs::path file = createReplicatorFile();
 
     // Create torrent file
+    //
     fs::path torrentFile = file.parent_path() / "info.torrent";
     outInfoHashOfSomeFile = createTorrentFile( file, torrentFile );
 
-    // emulate replicator side
+    // Emulate replicator side
     auto ltWrapper = createDefaultLibTorrentSession("127.0.0.1:5550");
     ltWrapper->addTorrentFileToSession( torrentFile, file );
 
-    // wait for the download to finish
+    // Wait for the download to finish
     std::unique_lock<std::mutex> lock(finishMutex);
     finishCondVar.wait( lock, []{return isFinished;} );
 }
 
+//
 // createReplicatorFile
+//
 fs::path createReplicatorFile() {
 
-    // create empty tmp folder for testing
+    // Create empty tmp folder for testing
+    //
     auto tmpFolder = fs::temp_directory_path() / "replicator_files";
     fs::remove_all( tmpFolder );
     fs::create_directories( tmpFolder );
 
-    // create file.bin
+    // Create file.bin
+    //
     fs::path fname = tmpFolder / "file.bin";
 
     std::vector<uint8_t> data(1024*1024/2);
@@ -134,6 +161,7 @@ fs::path createReplicatorFile() {
     std::ofstream file( fname );
     file.write( (char*) data.data(), data.size() );
 
+    // Return path to file
     return fname;
 }
 
