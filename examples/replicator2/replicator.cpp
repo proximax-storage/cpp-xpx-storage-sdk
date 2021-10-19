2e44fe445e1d084a44c6a93fec79e2fc8d79c834
Viktor.Markvardt <viktor.markvardt@gmail.com>
2021-10-19 17:46:29 +0300

    Fix replicator.cpp

--- examples/replicator2/replicator.cpp
+++ examples/replicator2/replicator.cpp
@@ -9,11 +9,13 @@
 
 #define REPLICATOR_NAME                 "replicator1"
 #define REPLICATOR_PRIVATE_KEY          "1000000000010203040501020304050102030405010203040501020304050102"
-#define REPLICATOR_ADDRESS              "192.168.0.100"
+#define REPLICATOR_ADDRESS              "192.168.0.102"
 #define REPLICATOR_PORT                 "5550"
 #define RPC_PORT                        5510
 #define REPLICATOR_ROOT_FOLDER          (std::string(getenv("HOME"))+"/111/replicator_root")
 #define REPLICATOR_SANDBOX_ROOT_FOLDER  (std::string(getenv("HOME"))+"/111/sandbox_root")
+#define EMULATOR_RPC_ADDRESS            "192.168.0.101"
+#define EMULATOR_RPC_PORT               5510
 
 namespace fs = std::filesystem;
 
@@ -29,7 +31,9 @@ int main()
                               REPLICATOR_PORT,
                               REPLICATOR_ROOT_FOLDER,
                               REPLICATOR_SANDBOX_ROOT_FOLDER,
-                              RPC_PORT );
+                              RPC_PORT,
+                              EMULATOR_RPC_ADDRESS,
+                              EMULATOR_RPC_PORT);
 
     replicator.run();
 
