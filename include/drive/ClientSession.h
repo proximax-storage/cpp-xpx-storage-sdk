853fa8f9f4296da1e8d3a1091d9bbfb7956eeec7
kyrylo <cyrillicw@gmail.com>
2021-12-30 17:55:01 +0200

    Endpoint Manager (not completed)

--- include/drive/ClientSession.h
+++ include/drive/ClientSession.h
@@ -285,7 +285,7 @@ protected:
         return m_receivedSize[peerPublicKey];
     }
     
-    virtual void onMessageReceived( const std::string& query, const std::string& message ) override
+    virtual void onMessageReceived( const std::string& query, const std::string& message, const boost::asio::ip::udp::endpoint& source ) override
     {
     }
 
