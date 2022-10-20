6f8cd032d320d769d2a36c93fdd561750dfccf48
changyc9928 <changyc9928@gmail.com>
2022-10-20 14:12:32 +0800

    Add more tests

---
+++ examples/supercontract-tests/testSupercontractRemove.cpp
@@ -0,0 +1,123 @@
+#include "TestEnvironment.h"
+#include "drive/Utils.h"
+#include "types.h"
+#include "utils.h"
+#include <fstream>
+#include <numeric>
+
+using namespace sirius::drive::test;
+
+namespace sirius::drive::test {
+
+/// change this macro for your test
+#define TEST_NAME SupercontractRemove
+
+#define ENVIRONMENT_CLASS JOIN(TEST_NAME, TestEnvironment)
+
+class ENVIRONMENT_CLASS
+    : public TestEnvironment {
+public:
+    ENVIRONMENT_CLASS(
+        int numberOfReplicators,
+        const std::string &ipAddr0,
+        int port0,
+        const std::string &rootFolder0,
+        const std::string &sandboxRootFolder0,
+        bool useTcpSocket,
+        int modifyApprovalDelay,
+        int downloadApprovalDelay,
+        int downloadRateLimit,
+        int startReplicator = -1)
+        : TestEnvironment(
+              numberOfReplicators,
+              ipAddr0,
+              port0,
+              rootFolder0,
+              sandboxRootFolder0,
+              useTcpSocket,
+              modifyApprovalDelay,
+              downloadApprovalDelay,
+              startReplicator,
+              true) {}
+};
+
+class TestHandlerRemove {
+
+public:
+    std::promise<void> p;
+    DriveKey m_driveKey;
+    uint64_t m_fileId;
+    uint64_t m_bytes;
+    ENVIRONMENT_CLASS &m_env;
+
+    TestHandlerRemove(ENVIRONMENT_CLASS
+                          &env)
+        : m_env(env) {}
+
+public:
+    void onAppliedStorageModifications(std::optional<ApplyStorageModificationsResponse> res) {
+        ASSERT_TRUE(res);
+        m_env.getFilesystem(m_driveKey, FilesystemRequest{[this](auto res) {
+                            }});
+    }
+
+    void onStorageHashEvaluated(std::optional<EvaluateStorageHashResponse> res) {
+        ASSERT_TRUE(res);
+        m_env.applyStorageManualModifications(m_driveKey, ApplyStorageModificationsRequest{true, [this](auto res) {
+                                                                                               onAppliedStorageModifications(res);
+                                                                                           }});
+    }
+
+    void onAppliedSandboxModifications(std::optional<ApplySandboxModificationsResponse> res) {
+        ASSERT_TRUE(res);
+        ASSERT_TRUE(res->m_success);
+        m_env.evaluateStorageHash(m_driveKey, EvaluateStorageHashRequest{[this](auto res) {
+                                      onStorageHashEvaluated(res);
+                                  }});
+    }
+
+    void onFileRemoved(std::optional<RemoveResponse> res) {
+        ASSERT_TRUE(res);
+        ASSERT_TRUE(res->m_success);
+        m_env.applySandboxManualModifications(m_driveKey, ApplySandboxModificationsRequest{true, [this](auto res) {
+                                                                                               onAppliedSandboxModifications(res);
+                                                                                           }});
+    }
+
+    void onSandboxModificationsInitiated(std::optional<InitiateSandboxModificationsResponse> res) {
+        ASSERT_TRUE(res);
+        m_env.removeFsTreeEntry(m_driveKey, RemoveRequest{"test.txt", [this](auto res) {
+                                                              onFileRemoved(res);
+                                                          }});
+    }
+
+    void onInitiatedModifications(std::optional<InitiateModificationsResponse> res) {
+        ASSERT_TRUE(res);
+        m_env.initiateManualSandboxModifications(m_driveKey, InitiateSandboxModificationsRequest{[this](auto res) {
+                                                     onSandboxModificationsInitiated(
+                                                         res);
+                                                 }});
+    }
+};
+
+TEST(SupercontractTest, TEST_NAME) {
+    fs::remove_all(ROOT_FOLDER);
+    EXLOG("");
+
+    ENVIRONMENT_CLASS env(
+        1, REPLICATOR_ADDRESS, PORT, DRIVE_ROOT_FOLDER,
+        SANDBOX_ROOT_FOLDER, USE_TCP, 1, 1, 1024 * 1024);
+
+    Key driveKey{{1}};
+    env.addDrive(driveKey, Key(), 100 * 1024 * 1024);
+
+    TestHandlerRemove handler(env);
+    handler.m_driveKey = driveKey;
+    env.initiateManualModifications(driveKey,
+                                    InitiateModificationsRequest{randomByteArray<Hash256>(), [&](auto res) { handler.onInitiatedModifications(res); }});
+
+    handler.p.get_future().wait();
+}
+
+#undef TEST_NAME
+} // namespace sirius::drive::test
