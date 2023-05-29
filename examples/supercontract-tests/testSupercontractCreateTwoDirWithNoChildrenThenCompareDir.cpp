aff889e6898e4a08e179d9c2c866d937327f9fb8
KokZheHua <KokZheHua@github.com>
2023-05-29 15:40:57 +0800

    testSupercontractCreateTwoDirWithNoChildrenThenCompareDir

---
+++ examples/supercontract-tests/testSupercontractCreateTwoDirWithNoChildrenThenCompareDir.cpp
@@ -0,0 +1,246 @@
+#include "TestEnvironment.h"
+#include "drive/Utils.h"
+#include "types.h"
+#include "utils.h"
+#include <fstream>
+#include <numeric>
+
+using namespace sirius::drive::test;
+
+namespace sirius::drive::test {
+
+    /// change this macro for your test
+#define TEST_NAME SupercontractCreateTwoDirWithNoChildrenThenCompareDir
+
+#define ENVIRONMENT_CLASS JOIN(TEST_NAME, TestEnvironment)
+
+    namespace {
+
+        class ENVIRONMENT_CLASS
+            : public TestEnvironment {
+        public:
+            ENVIRONMENT_CLASS(
+                int numberOfReplicators,
+                const std::string& ipAddr0,
+                int port0,
+                const std::string& rootFolder0,
+                const std::string& sandboxRootFolder0,
+                bool useTcpSocket,
+                int modifyApprovalDelay,
+                int downloadApprovalDelay,
+                int downloadRateLimit,
+                int startReplicator = -1)
+                : TestEnvironment(
+                    numberOfReplicators,
+                    ipAddr0,
+                    port0,
+                    rootFolder0,
+                    sandboxRootFolder0,
+                    useTcpSocket,
+                    modifyApprovalDelay,
+                    downloadApprovalDelay,
+                    startReplicator,
+                    true) {}
+        };
+
+        class TestCreateTwoDir {
+
+        public:
+            std::promise<void> p;
+            DriveKey m_driveKey;
+            uint64_t m_fileId;
+            ENVIRONMENT_CLASS& m_env;
+
+            TestCreateTwoDir(ENVIRONMENT_CLASS& env)
+                : m_env(env) {}
+
+        public:
+            void onReceivedFsTree(std::optional<FilesystemResponse> res) {
+                ASSERT_TRUE(res);
+                auto& fsTree = res->m_fsTree;
+                ASSERT_TRUE(fsTree.childs().size() == 2);
+                for (auto const& [_, child] : fsTree.childs()) {
+                    ASSERT_TRUE(isFolder(child));
+                    const auto& folder = getFolder(child);
+                    if (folder.name() == "tests2") {
+                        ASSERT_TRUE(folder.name() == "tests2");
+                        ASSERT_TRUE(folder.childs().size() == 0);
+                    }
+                    else {
+                        ASSERT_TRUE(folder.name() == "tests");
+                        ASSERT_TRUE(folder.childs().size() == 0);
+                    }
+                }
+                p.set_value();
+            }
+
+            void onAppliedStorageModifications(std::optional<ApplyStorageModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.getFilesystem(m_driveKey, FilesystemRequest{ [this](auto res) {
+                                        onReceivedFsTree(res);
+                                    } });
+            }
+
+            void onStorageHashEvaluated(std::optional<EvaluateStorageHashResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.applyStorageManualModifications(m_driveKey, ApplyStorageModificationsRequest{ true, [this](auto res) {
+                                                                                                       onAppliedStorageModifications(res);
+                                                                                                   } });
+            }
+
+            void onAppliedSandboxModifications(std::optional<ApplySandboxModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                ASSERT_TRUE(res->m_success);
+                m_env.evaluateStorageHash(m_driveKey, EvaluateStorageHashRequest{ [this](auto res) {
+                                              onStorageHashEvaluated(res);
+                                          } });
+            }
+
+            void onDirCreated2(std::optional<CreateDirectoriesResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.applySandboxManualModifications(m_driveKey, ApplySandboxModificationsRequest{ true, [this](auto res) {
+                                                                                                       onAppliedSandboxModifications(res);
+                                                                                                   } });
+            }
+			
+			void onDirCreated(std::optional<CreateDirectoriesResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.createDirectories(m_driveKey, CreateDirectoriesRequest{ "tests2", [this](auto res) { onDirCreated2(res); } });
+            }
+
+            void onSandboxModificationsInitiated(std::optional<InitiateSandboxModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.createDirectories(m_driveKey, CreateDirectoriesRequest{ "tests", [this](auto res) { onDirCreated(res); } });
+            }
+
+            void onInitiatedModifications(std::optional<InitiateModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.initiateManualSandboxModifications(m_driveKey, InitiateSandboxModificationsRequest{ [this](auto res) {
+                                                             onSandboxModificationsInitiated(
+                                                                 res);
+                                                         } });
+            }
+        };
+
+        class TestCompareFolderSize {
+		public:
+			std::promise<void> p;
+			DriveKey m_driveKey;
+			uint64_t m_fileId;
+			uint64_t m_bytes;
+            uint64_t m_size;
+            uint64_t m_size2;
+			ENVIRONMENT_CLASS& m_env;
+
+			TestCompareFolderSize(ENVIRONMENT_CLASS& env)
+				: m_env(env) {}
+
+		public:
+			void onReceivedFsTree(std::optional<FilesystemResponse> res) {
+                ASSERT_TRUE(res);
+                auto& fsTree = res->m_fsTree;
+                ASSERT_TRUE(fsTree.childs().size() == 2);
+                for (auto const& [_, child] : fsTree.childs()) {
+                    ASSERT_TRUE(isFolder(child));
+                    const auto& folder = getFolder(child);
+                    if (folder.name() == "tests2") {
+                        ASSERT_TRUE(folder.name() == "tests2");
+                        ASSERT_TRUE(folder.childs().size() == 0);
+                    }
+                    else {
+                        ASSERT_TRUE(folder.name() == "tests");
+                        ASSERT_TRUE(folder.childs().size() == 0);
+                    }
+                }
+                p.set_value();
+            }
+
+            void onAppliedStorageModifications(std::optional<ApplyStorageModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.getFilesystem(m_driveKey, FilesystemRequest{ [this](auto res) {
+                                        onReceivedFsTree(res);
+                                    } });
+            }
+
+            void onStorageHashEvaluated(std::optional<EvaluateStorageHashResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.applyStorageManualModifications(m_driveKey, ApplyStorageModificationsRequest{ true, [this](auto res) {
+                                                                                                       onAppliedStorageModifications(res);
+                                                                                                   } });
+            }
+
+            void onAppliedSandboxModifications(std::optional<ApplySandboxModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                ASSERT_TRUE(res->m_success);
+                m_env.evaluateStorageHash(m_driveKey, EvaluateStorageHashRequest{ [this](auto res) {
+                                              onStorageHashEvaluated(res);
+                                          } });
+            }
+
+			void onFileSize2(std::optional<FileSizeResponse> res) {
+				ASSERT_TRUE(res);
+                ASSERT_FALSE(res->m_success);
+				FileSizeResponse response = *res;
+				m_size2 = response.m_size;
+				ASSERT_EQ(m_size, m_size2);
+				m_env.applySandboxManualModifications(m_driveKey, ApplySandboxModificationsRequest{ true, [this](auto res) {
+																		   onAppliedSandboxModifications(res);
+																	   } });
+			}
+
+			void onFileSize(std::optional<FileSizeResponse> res) {
+				ASSERT_TRUE(res);
+                ASSERT_FALSE(res->m_success);
+				FileSizeResponse response = *res;
+				m_size = response.m_size;
+				m_env.getSize(m_driveKey, FileSizeRequest{"tests2", [this](auto res) {
+															   onFileSize2(res);
+														   }});
+			}
+
+            void onSandboxModificationsInitiated(std::optional<InitiateSandboxModificationsResponse> res) {
+                ASSERT_TRUE(res);
+				m_env.getSize(m_driveKey, FileSizeRequest{"tests", [this](auto res) {
+												   onFileSize(res);
+											   }});
+            }
+
+            void onInitiatedModifications(std::optional<InitiateModificationsResponse> res) {
+                ASSERT_TRUE(res);
+                m_env.initiateManualSandboxModifications(m_driveKey, InitiateSandboxModificationsRequest{ [this](auto res) {
+                                                             onSandboxModificationsInitiated(
+                                                                 res);
+                                                         } });
+            }
+		};
+
+        TEST(SupercontractTest, TEST_NAME) {
+            fs::remove_all(ROOT_FOLDER);
+            EXLOG("");
+
+            ENVIRONMENT_CLASS env(
+                1, REPLICATOR_ADDRESS, PORT, DRIVE_ROOT_FOLDER,
+                SANDBOX_ROOT_FOLDER, USE_TCP, 1, 1, 1024 * 1024);
+
+            Key driveKey{ {1} };
+            env.addDrive(driveKey, Key(), 100 * 1024 * 1024);
+
+            TestCreateTwoDir handler(env);
+            handler.m_driveKey = driveKey;
+            env.initiateManualModifications(driveKey,
+                InitiateModificationsRequest{ randomByteArray<Hash256>(), [&](auto res) { handler.onInitiatedModifications(res); } });
+
+            handler.p.get_future().wait();
+
+            TestCompareFolderSize handler2(env);
+            handler2.m_driveKey = driveKey;
+            env.initiateManualModifications(driveKey,
+                InitiateModificationsRequest{ randomByteArray<Hash256>(), [&](auto res) { handler2.onInitiatedModifications(res); } });
+
+            handler2.p.get_future().wait();
+        }
+    } // namespace
+
+#undef TEST_NAME
+} // namespace sirius::drive::test
+
