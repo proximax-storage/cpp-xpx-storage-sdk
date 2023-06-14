7091e231130d3e39dda1ae220d3964f045bafed8
KokZheHua <KokZheHua@github.com>
2023-06-14 17:05:29 +0800

    testSupercontractCreawteDirWithAFileAndCreateAnotherDirWithNoChildrenThenCompareDir

--- examples/supercontract-tests/testSupercontractCreawteDirWithAFileAndCreateAnotherDirWithNoChildrenThenCompareDir.cpp
+++ examples/supercontract-tests/testSupercontractCreawteDirWithAFileAndCreateAnotherDirWithNoChildrenThenCompareDir.cpp
@@ -251,9 +251,6 @@ namespace sirius::drive::test {
 			void onFileSize2(std::optional<FileSizeResponse> res) {
 				ASSERT_TRUE(res);
                 ASSERT_FALSE(res->m_success);
-				FileSizeResponse response = *res;
-				m_size2 = response.m_size;
-				ASSERT_EQ(m_size, m_size2);
 				m_env.applySandboxManualModifications(m_driveKey, ApplySandboxModificationsRequest{ true, [this](auto res) {
 																		   onAppliedSandboxModifications(res);
 																	   } });
@@ -262,8 +259,6 @@ namespace sirius::drive::test {
 			void onFileSize(std::optional<FileSizeResponse> res) {
 				ASSERT_TRUE(res);
                 ASSERT_FALSE(res->m_success);
-				FileSizeResponse response = *res;
-				m_size = response.m_size;
 				m_env.getSize(m_driveKey, FileSizeRequest{"tests2", [this](auto res) {
 															   onFileSize2(res);
 														   }});
