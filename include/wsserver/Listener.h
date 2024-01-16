f955a045afa7945992ee24fad6da833120f8e424
Arvind Siva <arvindsiva11@gmail.com>
2024-01-16 15:16:21 +0800

    Create wsserver shared lib

--- include/wsserver/Listener.h
+++ include/wsserver/Listener.h
@@ -26,62 +26,63 @@ namespace pt = boost::property_tree;    // from <boost/property_tree>
 
 void fail(beast::error_code ec, char const* what);
 
-
-// Accepts incoming connections and launches the Sessions
-class Listener : public std::enable_shared_from_this<Listener>
-{
-    net::io_context& ioc;
-    tcp::acceptor acceptor;
-    net::io_context::strand strand;
-
-public:
-    Listener(
-        net::io_context& ioc,
-        tcp::endpoint endpoint)
-        : ioc(ioc)
-        , acceptor(ioc)
-        , strand(ioc)
+namespace sirius { namespace wsserver {
+    // Accepts incoming connections and launches the Sessions
+    class Listener : public std::enable_shared_from_this<Listener>
     {
-        beast::error_code ec;
+        net::io_context& ioc;
+        tcp::acceptor acceptor;
+        net::io_context::strand strand;
 
-        // Open the acceptor
-        acceptor.open(endpoint.protocol(), ec);
-        if(ec)
+    public:
+        Listener(
+            net::io_context& ioc,
+            tcp::endpoint endpoint)
+            : ioc(ioc)
+            , acceptor(ioc)
+            , strand(ioc)
         {
-            fail(ec, "open");
-            return;
+            beast::error_code ec;
+
+            // Open the acceptor
+            acceptor.open(endpoint.protocol(), ec);
+            if(ec)
+            {
+                fail(ec, "open");
+                return;
+            }
+
+            // Allow address reuse
+            acceptor.set_option(net::socket_base::reuse_address(true), ec);
+            if(ec)
+            {
+                fail(ec, "set_option");
+                return;
+            }
+
+            // Bind to the server address
+            acceptor.bind(endpoint, ec);
+            if(ec)
+            {
+                fail(ec, "bind");
+                return;
+            }
+
+            // Start listening for connections
+            acceptor.listen(
+                net::socket_base::max_listen_connections, ec);
+            if(ec)
+            {
+                fail(ec, "listen");
+                return;
+            }
         }
 
-        // Allow address reuse
-        acceptor.set_option(net::socket_base::reuse_address(true), ec);
-        if(ec)
-        {
-            fail(ec, "set_option");
-            return;
-        }
-
-        // Bind to the server address
-        acceptor.bind(endpoint, ec);
-        if(ec)
-        {
-            fail(ec, "bind");
-            return;
-        }
-
-        // Start listening for connections
-        acceptor.listen(
-            net::socket_base::max_listen_connections, ec);
-        if(ec)
-        {
-            fail(ec, "listen");
-            return;
-        }
-    }
-
-    // Start accepting incoming connections
-    void run();
-    
-private:
-    void doAccept();
-    void onAccept(beast::error_code ec, tcp::socket socket);
-};
+        // Start accepting incoming connections
+        void run();
+        
+    private:
+        void doAccept();
+        void onAccept(beast::error_code ec, tcp::socket socket);
+    };
+}}
--- src/wsserver/Listener.cpp
+++ src/wsserver/Listener.cpp
@@ -1,33 +1,35 @@
 #include "wsserver/Listener.h"
 #include "wsserver/InSession.h"
 
-void Listener::run()
-{
-    doAccept();
-}
-
-void Listener::doAccept()
-{
-    // The new connection gets its own strand
-    acceptor.async_accept(
-        net::make_strand(ioc),
-        beast::bind_front_handler(
-            &Listener::onAccept,
-            shared_from_this()));
-}
-
-void Listener::onAccept(beast::error_code ec, tcp::socket socket)
-{
-    if(ec)
+namespace sirius { namespace wsserver {
+    void Listener::run()
     {
-        fail(ec, "accept");
-    }
-    else
-    {
-        // Create the Session and run it
-        std::make_shared<InSession>(std::move(socket), strand)->run();
+        doAccept();
     }
 
-    // Accept another connection
-    doAccept();
-}
+    void Listener::doAccept()
+    {
+        // The new connection gets its own strand
+        acceptor.async_accept(
+            net::make_strand(ioc),
+            beast::bind_front_handler(
+                &Listener::onAccept,
+                shared_from_this()));
+    }
+
+    void Listener::onAccept(beast::error_code ec, tcp::socket socket)
+    {
+        if(ec)
+        {
+            fail(ec, "accept");
+        }
+        else
+        {
+            // Create the Session and run it
+            std::make_shared<InSession>(std::move(socket), strand)->run();
+        }
+
+        // Accept another connection
+        doAccept();
+    }
+}}
